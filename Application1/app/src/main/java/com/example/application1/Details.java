90e45dfa0183d7c3d7a91b241df7e2d83dd2090e
Zhadyra98 <oralkhanova.zhadyra@gmail.com>
2019-12-08 13:53:14 +0600

    Fragment is added

--- Application1/app/src/main/java/com/example/application1/Details.java
+++ Application1/app/src/main/java/com/example/application1/Details.java
@@ -1,11 +1,16 @@
 package com.example.application1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Details extends AppCompatActivity {
     TextView storyContent;
@@ -14,6 +19,7 @@ public class Details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
         storyContent = findViewById(R.id.contentOfStory);
         Intent i = getIntent();
         String title = i.getStringExtra("titleOfStory");
@@ -22,6 +28,34 @@ public class Details extends AppCompatActivity {
         storyContent.setText(content);
         storyContent.setMovementMethod(new ScrollingMovementMethod());
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFrag()).commit();
+        }
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFrag();
+                    break;
+                case R.id.nav_second:
+                    selectedFragment = new DetailsFrag();
+                    break;
+                case R.id.nav_third:
+                    selectedFragment = new RateFrag();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
